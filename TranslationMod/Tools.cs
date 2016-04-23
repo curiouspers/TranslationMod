586d7bf58eec3e8fe56d029b56fb9176f36d20a0
curiouspers <curiouspers@gmail.com>
2016-04-23 17:50:10 +0400

    After auto dictionary parse from nota

--- TranslationMod/Tools.cs
+++ TranslationMod/Tools.cs
@@ -420,7 +420,7 @@ namespace TranslationMod
 
     public static class Tools
     {
-        public static Regex reNumber = new Regex("[\\d +-.,:]+", RegexOptions.Compiled);
+        public static Regex reNumber = new Regex("[\\d +-.,:]+(am|pm)*", RegexOptions.Compiled);
         public static char[] numberArr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '+', '-', '.', ',', ':' };
         public static string ReplaceFirst(this string text, string search, string replace)
         {
